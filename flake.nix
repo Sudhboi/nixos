c9cbf509eb22d66a79db643fa0a711df86587111
Sudhir Krisna <sudhboiii@gmail.com>
2026-02-19 17:45:35 -0400

    Zen

--- flake.nix
+++ flake.nix
@@ -3,11 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = inputs @ {
+      self,
+      nixpkgs,
+      home-manager,
+      zen-browser,
+      ...
+  }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
@@ -23,6 +38,7 @@
       sudhirk = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./users/sudhirk/home.nix ];
+        extraSpecialArgs = { inherit inputs; };
       };
     };
   };
