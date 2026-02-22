0c5c8ee762d99de10248901140b5c3c78046f628
Sudhir Krisna <sudhboiii@gmail.com>
2026-02-22 17:38:45 -0400

    finished sync commands

--- users/sudhirk/scripts/sync_boot.sh
+++ users/sudhirk/scripts/sync_boot.sh
@@ -1,12 +1,10 @@
 function giterit () {
-    cd $1
-    notify-send -t 500 "$(git pull): $1"
+    (
+        cd $1
+        notify-send -t 500 "$(git pull): $1"
+    ) &
 }
 
-read dir
-while [[ "$dir" != "" ]]; do
+while read -r dir && [[ -n "$dir" ]]; do
     giterit $dir
-    read dir
 done
-
-notify-send -t 1000 "Sync Complete!"
--- users/sudhirk/scripts/sync_poweroff.sh
+++ users/sudhirk/scripts/sync_poweroff.sh
@@ -1,14 +1,17 @@
 function gitit () {
-    cd $1
-    git add .
-    git commit -m "$(date) - Sync Commit"
-    git push
+    (
+        cd "$1"
+        if (! git diff --quiet) || (! git diff --quiet --cached); then
+            git add .
+            git commit -m "$(date) - Sync Commit" >> "/home/sudhirk/.logs/$(basename "$1").log"
+            git push >> "/home/sudhirk/.logs/$(basename "$1").log"
+        fi
+    ) &
 }
 
-read dir
-while [[ "$dir" != "" ]]; do
+while read -r dir && [[ -n "$dir" ]]; do
     gitit $dir
-    read dir
 done
 
+wait
 poweroff
