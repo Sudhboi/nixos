abd36d26de4f505c1ab5f3db664e8aeb2da8ce04
Sudhir Krisna <sudhboiii@gmail.com>
2026-02-28 14:08:46 -0400

    Sat 28 Feb 2026 02:08:46 PM AST - Sync Commit

--- users/sudhirk/desktop/waybar/waybar.nix
+++ users/sudhirk/desktop/waybar/waybar.nix
@@ -5,5 +5,6 @@
     enable = true;
   };
   xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
+  # xdg.configFile."waybar/config2.jsonc".source = ./config2.jsonc;
   xdg.configFile."waybar/style.css".source = ./style.css;
 }
