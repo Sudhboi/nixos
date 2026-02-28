f5c71d1c36e3d8ae73ca1c0136d292ecd6b37a5e
Sudhir Krisna <sudhboiii@gmail.com>
2026-02-28 01:16:19 -0400

    Sat 28 Feb 2026 01:16:19 AM AST - Sync Commit

--- users/sudhirk/home.nix
+++ users/sudhirk/home.nix
@@ -68,6 +68,7 @@ in
     tex
     vicinae
     heroic
+    tikzit
     nautilus
     # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
