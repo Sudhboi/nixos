ed13b6b1552f0a90f1d3dd68bb64bc752ce5d8b1
Sudhir Krisna <sudhboiii@gmail.com>
2026-02-28 20:14:23 -0400

    Sat 28 Feb 2026 08:14:23 PM AST - Sync Commit

--- users/sudhirk/scripts/sync_poweroff.sh
+++ users/sudhirk/scripts/sync_poweroff.sh
@@ -1,11 +1,13 @@
-function gitit () {
+function gitit() {
     (
         cd "$1"
         git add .
-        git commit -m "$(date) - Sync Commit" >> "/home/sudhirk/.logs/$(basename "$1").log"
-        git push >> "/home/sudhirk/.logs/$(basename "$1").log"
+        git commit -m "$(date) - Sync Commit" >>"/home/sudhirk/.logs/$(basename "$1").log"
+        git push >>"/home/sudhirk/.logs/$(basename "$1").log"
     ) &
 }
+
+notify-send "Syncing..."
 
 while read -r dir && [[ -n "$dir" ]]; do
     gitit $dir
